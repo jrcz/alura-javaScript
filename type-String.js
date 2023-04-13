9a8314eab76e26280ac95b4412463279c61a3716
Joao Zanetti <joao.zanetti@bb.com.br>
2023-04-13 12:53:26 -0300

    Comit inicial do curso basico de JS

---
+++ type-String.js
@@ -0,0 +1,23 @@
+//Preparando arquivo para um commit
+
+const texto1 = "teste texto 0123"
+
+console.log(texto1)
+
+const numeroString = "4"
+const numeroString1 = "2"
+
+console.log(numeroString / numeroString1) //isso é muito estranho
+console.log(numeroString + numeroString1) //para teste do git
+
+
+var cidade = "belo horizonte"
+const input = "BELO HORIZONTE"
+
+console.log(cidade === input)
+
+console.log(cidade.toUpperCase() == input)
+
+console.log(cidade.length)
+
+
---
+++ type-number.js
@@ -0,0 +1,9 @@
+// tipo number
+
+const n1 = 1;
+const n2 = 2;
+const zero = 0;
+
+const n3 = n1 / n2;
+
+console.log(`O numero digitado é: ${n3}`);
